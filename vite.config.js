b8ea4b67feb6ca2b233d2209a21dcb0374260495
jonathan-vitangeli <jvitangeli@americavirtualsa.com>
2026-04-28 18:19:59 -0300

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
@@ -7,6 +8,14 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        portfolio: resolve(__dirname, 'portfolio.html'),
+        proyecto: resolve(__dirname, 'proyecto.html')
+      }
+    }
   }
 })
